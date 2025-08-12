f45a369186edb44a60f4f6e3bfb5097f3032077e
oldmo0415 <150570235+oldmo0415@users.noreply.github.com>
2025-08-13 00:33:45 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import MarvelMoviesSite from './MarvelMoviesSite'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MarvelMoviesSite />
+  </React.StrictMode>,
+)
